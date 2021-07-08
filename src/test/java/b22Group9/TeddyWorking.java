0678852338aaf887d5ee227c338f1f1515ce51e7
Stanislav Pryimak <st.pryimak@gmail.com>
2021-07-08 14:34:39 -0500

    Update TeddyWorking.java
    Very important addition.

--- src/test/java/b22Group9/TeddyWorking.java
+++ src/test/java/b22Group9/TeddyWorking.java
@@ -3,5 +3,6 @@ package b22Group9;
 public class TeddyWorking {
     public static void main(String[] args) {
         System.out.println("Our group is THE BEST");
+        System.out.println("No doubts!");
     }
 }
