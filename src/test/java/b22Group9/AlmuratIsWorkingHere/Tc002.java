7d05be4341d2379f98180eded922882b664ebe6c
Almurat98 <almurat908@gmail.com>
2021-07-07 21:09:35 -0500

    push again 3

--- src/test/java/b22Group9/AlmuratIsWorkingHere/Tc002.java
+++ src/test/java/b22Group9/AlmuratIsWorkingHere/Tc002.java
@@ -4,4 +4,6 @@ public class Tc002 {
     //try to push again
     // why is failing to push
     // conflict resolved and pushed new commit
+    // do not push on deleted branch
+    // create new branch to commit
 }
