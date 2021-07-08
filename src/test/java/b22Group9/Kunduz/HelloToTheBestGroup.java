3bd25e2c109a9e6683127915953f819f1f87f5fd
KunduzKoyu <kunduzazamat@gmail.com>
2021-07-07 20:11:39 -0500

    workin gin Almurats code

--- src/test/java/b22Group9/Kunduz/HelloToTheBestGroup.java
+++ src/test/java/b22Group9/Kunduz/HelloToTheBestGroup.java
@@ -3,5 +3,6 @@ package b22Group9.Kunduz;
 public class HelloToTheBestGroup {
     public static void main(String[] args) {
         System.out.println("Enjoy today's class everyone!");
+        System.out.println("Changes made");
     }
 }
