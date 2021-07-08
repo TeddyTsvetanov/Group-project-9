2562fb0f0cf00bd9f940e45d2a463fb3e4356a28
KunduzKoyu <kunduzazamat@gmail.com>
2021-07-07 20:49:25 -0500

    conflict resolved I guess

--- src/test/java/b22Group9/Kunduz/HelloToTheBestGroup.java
+++ src/test/java/b22Group9/Kunduz/HelloToTheBestGroup.java
@@ -4,6 +4,6 @@ public class HelloToTheBestGroup {
     public static void main(String[] args) {
         System.out.println("Enjoy today's class everyone!");
         System.out.println("Changes made");
-        System.out.println("coflicting here with Almurat:)");
+        System.out.println("conflicting here with Almurat:)");
     }
 }
