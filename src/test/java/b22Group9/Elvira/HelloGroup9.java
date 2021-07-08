055007fbb9b40e0224bfccdf7e3792420f9ff57c
ElviraDemir <ismayilova.elvira@gmail.com>
2021-07-07 20:10:10 -0500

    Working on conflicting file change with my team

--- src/test/java/b22Group9/Elvira/HelloGroup9.java
+++ src/test/java/b22Group9/Elvira/HelloGroup9.java
@@ -3,5 +3,6 @@ package b22Group9.Elvira;
 public class HelloGroup9 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Awesome conflict created by me");
     }
 }
