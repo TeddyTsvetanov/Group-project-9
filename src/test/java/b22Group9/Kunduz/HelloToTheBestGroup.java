493237e2b0f8f0d9c7598ca729cd794613b79538
KunduzKoyu <kunduzazamat@gmail.com>
2021-07-07 20:13:10 -0500

    conflict

--- src/test/java/b22Group9/Kunduz/HelloToTheBestGroup.java
+++ src/test/java/b22Group9/Kunduz/HelloToTheBestGroup.java
@@ -4,5 +4,6 @@ public class HelloToTheBestGroup {
     public static void main(String[] args) {
         System.out.println("Enjoy today's class everyone!");
         System.out.println("Changes made");
+        System.out.println("coflicting here with Almurat:)");
     }
 }
