5d0515f41e1ba7245984bae73e296b44376f6958
Dwolday <danaittewolde@yahoo.com>
2021-07-10 19:13:50 -0500

    commit is all done

--- src/test/java/b22Group9/DeeDee/HelloEveryone.java
+++ src/test/java/b22Group9/DeeDee/HelloEveryone.java
@@ -4,6 +4,7 @@ public class HelloEveryone {
     public static void main(String[] args) {
         System.out.println("Hello Hello");
         System.out.println("Murodil is the BEST but needs to get to the point ASAP");
+        System.out.println("EAST AFRICAN IN THE GIIIIITTTTT ;)(;");
 
     }
 
