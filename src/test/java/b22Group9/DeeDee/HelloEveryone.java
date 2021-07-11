22362694310789745fdc8ebedfc32f6e616e198e
Dwolday <danaittewolde@yahoo.com>
2021-07-10 19:12:48 -0500

    second commit

--- src/test/java/b22Group9/DeeDee/HelloEveryone.java
+++ src/test/java/b22Group9/DeeDee/HelloEveryone.java
@@ -3,6 +3,7 @@ package b22Group9.DeeDee;
 public class HelloEveryone {
     public static void main(String[] args) {
         System.out.println("Hello Hello");
+        System.out.println("Murodil is the BEST but needs to get to the point ASAP");
 
     }
 
