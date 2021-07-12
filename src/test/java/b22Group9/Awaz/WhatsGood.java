b2e4db31aa5cd082a7d29c53ac319c7339198a2b
Awaz <awazfanwar@gmail.com>
2021-07-11 22:13:11 -0500

    work 2

--- src/test/java/b22Group9/Awaz/WhatsGood.java
+++ src/test/java/b22Group9/Awaz/WhatsGood.java
@@ -3,5 +3,6 @@ package b22Group9.Awaz;
 public class WhatsGood {
     public static void main(String[] args) {
         System.out.println("Hey group");
+        System.out.println("The princess is heree");
     }
 }
